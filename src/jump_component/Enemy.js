16f2c9533a9a9e6f86d24399e8959d6c1295d8d2
fullgon <mbj0119@gmail.com>
2021-12-29 20:21:19 +0900

    complete enemyArr moving

--- src/jump_component/Enemy.js
+++ src/jump_component/Enemy.js
@@ -4,15 +4,14 @@ import dinoImg from "../image/dino.png"
 function Enemy({canvas, isStart}){
     let requestAnimationRef = useRef(null);
 
+    const enemyArr = [];
     const image = new Image();
 
     let move = 0;
-    const preset = {
-        x : 100,
-        y : 100,
-        width : 50,
-        height : 60
-    }
+    let frame = 0;
+    let index = 0;
+
+    const MOVE_SPEED = 2;
 
     useEffect(() => {
         if(canvas !== null){
@@ -29,12 +28,52 @@ function Enemy({canvas, isStart}){
     },[isStart])
 
     const render = () =>{
-
-        const context = canvas.getContext('2d');
         
-        /*context.fillStyle = 'green';
-        context.fillRect(preset.x, preset.y, preset.width, preset.height);*/
-        move += 2;
+        const context = canvas.getContext('2d');
+        if(frame % 60 === 0){//60프레임당 1번
+            const image = new Image();//이미지 객체
+            image.src = dinoImg;
+            const enemy ={//enemy 객체
+                image : image,
+                move : 0,
+                x : 500,
+                y : 100,
+                width : 50,
+                height : 60
+            }
+
+            enemyArr.push(enemy);//enemyArr에 enemy객체 추가
+            console.log(enemyArr);
+        }
+
+        enemyArr.map((enemy, index, arr) =>{
+
+            if(enemy.x + enemy.width - enemy.move >= 0){
+                /*enemy.image.onload = () =>{
+                    context.clearRect(0,0,canvas.width,canvas.height);
+                    context.drawImage(enemy.image, enemy.x - enemy.move, enemy.y, enemy.width, enemy.height);
+                    enemy.move += 2;
+                    console.log(enemy.move)
+                }*/
+                    context.clearRect(enemy.x- enemy.move, enemy.y, enemy.width + MOVE_SPEED, enemy.height);
+                    context.drawImage(enemy.image, enemy.x - enemy.move, enemy.y, enemy.width, enemy.height);
+                    enemy.move += MOVE_SPEED;
+                
+
+            }
+            else{
+                console.log(`삭제`);
+                console.log(arr[0], arr[1], arr[2]);
+                arr.shift();
+                
+                arr[index].move += MOVE_SPEED;
+                console.log(arr[0]);
+            }
+        }) 
+
+
+
+        /*move += 2;
         image.src = dinoImg;
         if(preset.x + preset.width - move >= 0){
             image.onload = () =>{
@@ -43,14 +82,14 @@ function Enemy({canvas, isStart}){
             }
         }
         else{
+            //배열에서 첫번째 요소 삭제
             move = 0;
         }
+        */
         
+        frame += 1;
+
         
-
-        //if(move === 50)
-          //  move = 0;
-
         requestAnimationRef.current = requestAnimationFrame(render);
     }
 
